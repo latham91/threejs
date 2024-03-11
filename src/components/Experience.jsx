15aee4169b2e384715db1bda23033da255007ebf
Aaron Latham <latham91@icloud.com>
2024-03-10 20:00:00 -0600

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,14 +3,14 @@ import { Office } from "./Office";
 import { Overlay } from "./Overlay";
 
 export default function Experience() {
-    return (
-        <>
-            <ambientLight intensity={1.2} />
-            <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
-            <ScrollControls pages={3} damping={0.25}>
-                <Overlay />
-                <Office />
-            </ScrollControls>
-        </>
-    );
+  return (
+    <>
+      <ambientLight intensity={1.3} />
+      <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
+      <ScrollControls pages={3} damping={0.25}>
+        <Overlay />
+        <Office />
+      </ScrollControls>
+    </>
+  );
 }
